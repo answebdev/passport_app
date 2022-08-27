3b594ffc2e7fcf90f798ad838bea4b85b12275e9
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-26 19:57:14 -0700

    add Passport

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,37 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
+const cors = require('cors');
+const passportSetup = require('./passport')
+const passport = require('passport');
 const app = express();
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['hoodwink'],
+    // Calculate to one day:
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure CORS
+// CORS allows us to make requests from any client server (e.g., React app)
+app.use(
+  cors({
+    // Client server:
+    origin: 'http:localhost/3000',
+    methods: 'GET,POST,PUT,DELETE',
+    // Setting 'credentials' to 'true' allows us to send sessions through our client server requests
+    credentials: true,
+  })
+);
+
+// Authentication Methods (from Passport docs, under 'Strategies' - see video around 44:31: https://www.youtube.com/watch?v=7K9kDrtc4S8 )
+// Google
 
 app.listen('5000', () => {
   console.log('Server is running!');
---
+++ backend/passport.js
@@ -0,0 +1,54 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const GOOGLE_CLIENT_ID =
+  '671201144777-4lfaf8rqvsgsbhvbgcj3578aq8angb48.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://www.example.com/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // 'profile' refers to user information: user id, user name, profile picture, etc.
+      done(null, profile);
+    }
+
+    // If you use a database (e.g., MongoDB), use these lines:
+    // function (accessToken, refreshToken, profile, cb) {
+    //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //     return cb(err, user);
+    //   });
+    // }
+
+    // If you use a database (e.g., MongoDB), use these lines:
+    // function (accessToken, refreshToken, profile, cb) {
+    //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //     return cb(err, user);
+    //   });
+    // }
+
+    // This is an example of what it looks like if you use MongoDB (something like this)
+    // function (accessToken, refreshToken, profile, cb) {
+    //   const user = {
+    //     username: profile.displayName,
+    //     avatar: profile.photos[0],
+    //   };
+    //   // Save the user
+    //   user.
+    // }
+  )
+);
+
+// Since sessions are used in thie app, we need to serialize and de-serialize the user to pass our sessions.
+// If you use sessions, you have to use these functions:
+passport.serializeUser((user, done) => {
+  done, user;
+});
+
+passport.deserializeUser((user, done) => {
+  done, user;
+});
